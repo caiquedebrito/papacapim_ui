75d5c970b7e851d577d419ae17ff56795096a3e1
Caique <caiquedebritoo@gmail.com>
2025-03-29 18:35:00 -0300

    feat: Implement routing with GoRouter and define application routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:papacapim_ui/constants/app_colors.dart';
-import 'package:papacapim_ui/screens/login_screen.dart';
-
-// ThemeData appTheme = ThemeData(
-//   primaryColor: AppColors.green, // Cor principal (verde)
-//   scaffoldBackgroundColor: AppColors.black, // Cor de fundo (preto)
-//   textTheme: const TextTheme(
-//     bodyLarge: TextStyle(color: Colors.white),
-//     bodyMedium: TextStyle(color: Colors.white),
-//     titleLarge: TextStyle(color: Colors.white),
-//   ),
-//   appBarTheme: AppBarTheme(
-//     backgroundColor: AppColors.black, // Cor da AppBar (fundo preto)
-//     foregroundColor: AppColors.green, // Cor do texto da AppBar (verde)
-//     elevation: 0, // Sem sombra
-//   ),
-//   bottomNavigationBarTheme: BottomNavigationBarThemeData(
-//     backgroundColor: AppColors.black, // Cor de fundo do BottomNavigation
-//     selectedItemColor: AppColors.green, // Cor do item selecionado (verde)
-//     unselectedItemColor: Colors.white, // Cor do item não selecionado
-//     showUnselectedLabels: true, // Mostrar rótulos dos itens não selecionados
-//     elevation: 8, // Elevação (sombra)
-//   ),
-//   buttonTheme: ButtonThemeData(
-//     buttonColor: AppColors.green, // Cor dos botões
-//     textTheme: ButtonTextTheme.primary, // Cor do texto no botão
-//   ),
-//   visualDensity: VisualDensity.adaptivePlatformDensity,
-// );
+import 'package:papacapim_ui/routing/router.dart';
 
 void main() {
   runApp(const MainApp());
@@ -38,7 +11,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Papacapim App',
       theme: ThemeData(
         colorScheme: ColorScheme.dark(
@@ -46,7 +19,7 @@ class MainApp extends StatelessWidget {
           secondary: AppColors.green,
         ),
       ),
-      home: const LoginScreen(),
+      routerConfig: router(),
     );
   }
 }
--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -1,10 +1,20 @@
 
+import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 import 'package:papacapim_ui/routing/routes.dart';
 import 'package:papacapim_ui/screens/cadastro_screen.dart';
+import 'package:papacapim_ui/screens/edit_profile_screen.dart';
+import 'package:papacapim_ui/screens/feed_screen.dart';
 import 'package:papacapim_ui/screens/login_screen.dart';
+import 'package:papacapim_ui/screens/my_profile_screen.dart';
+import 'package:papacapim_ui/screens/new_post_screen.dart';
+import 'package:papacapim_ui/screens/profile_screen.dart';
+import 'package:papacapim_ui/screens/replies_screen.dart';
+import 'package:papacapim_ui/screens/search_screen.dart';
 
 GoRouter router() => GoRouter(
+  initialLocation: Routes.login,
+  redirect: _redirect,
   routes: [
     GoRoute(
       path: Routes.login,
@@ -18,6 +28,51 @@ GoRouter router() => GoRouter(
         return const CadastroScreen();
       }
     ),
-    
+    GoRoute(
+      path: Routes.feed,
+      builder: (context, state) {
+        return const FeedScreen();
+      }
+    ),
+    GoRoute(
+      path: Routes.my_profile,
+      builder: (context, state) {
+        return const MyProfileScreen();
+      }
+    ),
+    GoRoute(
+      path: Routes.my_profile_edit,
+      builder: (context, state) {
+        return const EditProfileScreen();
+      }
+    ),
+    GoRoute(
+      path: Routes.new_post,
+      builder: (context, state) {
+        return const NewPostScreen();
+      }
+    ),
+    // GoRoute(
+    //   path: Routes.replies,
+    //   builder: (context, state) {
+    //     return const RepliesScreen();
+    //   }
+    // ),
+    GoRoute(
+      path: Routes.search,
+      builder: (context, state) {
+        return const SearchScreen();
+      }
+    ),
+    GoRoute(
+      path: Routes.profile,
+      builder: (context, state) {
+        return const ProfileScreen();
+      },
+    )
   ]
 );
+
+Future<String?> _redirect(BuildContext context, GoRouterState state) async {
+  return Routes.feed;
+}
