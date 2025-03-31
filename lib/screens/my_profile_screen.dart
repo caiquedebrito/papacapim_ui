ee4718fba3d85bb867cf365fd1deb2293cd6417d
Caique De Brito <caiquedebritoo@gmail.com>
2025-03-31 16:00:23 -0300

    feat: Implement logout functionality in MyProfileScreen and add GlobalSession management

--- lib/screens/my_profile_screen.dart
+++ lib/screens/my_profile_screen.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:papacapim_ui/components/follower_card.dart';
 import 'package:papacapim_ui/components/post_card.dart';
 import 'package:papacapim_ui/screens/login_screen.dart';
+import 'package:papacapim_ui/states/global_state.dart';
 import 'edit_profile_screen.dart';
 import '../components/bottom_navegation.dart';
 
@@ -108,6 +109,7 @@ class _ProfileScreenState extends State<MyProfileScreen> {
                             ),
                             IconButton(
                               onPressed: () {
+                                GlobalSession().logout();
                                 context.go('/login');
                               },
                               icon: Icon(Icons.logout, color: Theme.of(context).colorScheme.secondary,)
