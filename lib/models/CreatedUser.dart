938422a2cbdb47a054af7251b357ccddb406346c
Caique De Brito <caiquedebritoo@gmail.com>
2025-03-31 16:00:02 -0300

    feat: Remove id from Createduser model and update user creation logic to use GoRouter for navigation

--- lib/models/CreatedUser.dart
+++ lib/models/CreatedUser.dart
@@ -1,16 +1,14 @@
 class Createduser {
-  final int id;
   final String login;
   final String name;
   final String created_at;
   final String updated_at;
 
-  Createduser({required this.id, required this.login, required this.name, required this.created_at, required this.updated_at}); 
+  Createduser({required this.login, required this.name, required this.created_at, required this.updated_at}); 
 
   factory Createduser.fromJson(Map<String, dynamic> json) {
     return switch (json) {
-      {'id': int id, 'login': String login, 'name': String name, 'created_at': String created_at, 'updated_at': String updated_at} => Createduser(
-        id: id,
+      {'login': String login, 'name': String name, 'created_at': String created_at, 'updated_at': String updated_at} => Createduser(
         login: login, 
         name: name,
         created_at: created_at,
